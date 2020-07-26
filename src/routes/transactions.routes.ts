2fb0fa5d57671271f2394aac41f12f11c6cb3887
Gustavo <gustavoplensack@gmail.com>
2020-07-25 21:31:53 -0300

    feature: delete transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -4,7 +4,7 @@ import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -24,9 +24,9 @@ transactionsRouter.get('/', async (request, response) => {
 transactionsRouter.post('/', async (request, response) => {
   const { title, value, type, category } = request.body;
 
-  const transactionService = new CreateTransactionService();
+  const createTransactionService = new CreateTransactionService();
 
-  const newTransaction = await transactionService.execute({
+  const newTransaction = await createTransactionService.execute({
     title,
     value,
     type,
@@ -37,7 +37,13 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransactionService = new DeleteTransactionService();
+
+  await deleteTransactionService.execute({ id });
+
+  response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionRepo = getCustomRepository(TransactionsRepository);
+
+    await transactionRepo.delete(id);
   }
 }
 
